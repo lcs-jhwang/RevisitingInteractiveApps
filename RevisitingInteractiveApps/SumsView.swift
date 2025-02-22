3f09b8f88b99b73018ae39dc7201bb9d0a9a5b67
Julien Hwang <jhwang@lcs.on.ca>
2025-02-21 20:22:35 -0500

    Made the interface of entering the nubers to sum

---
+++ RevisitingInteractiveApps/SumsView.swift
@@ -0,0 +1,46 @@
+//
+//  SumsView.swift
+//  RevisitingInteractiveApps
+//
+//  Created by Julien Hwang on 21/2/2025.
+//
+
+import SwiftUI
+
+struct SumsView: View {
+    
+    //MARK: Stored properties
+    @State var firstNumber: Int = 1
+    @State var secondNumber: Int = 2
+    
+    //MARK: Computed properties
+    
+    
+    
+    var body: some View {
+        VStack{
+            HStack{
+                Spacer()
+                Text("\(firstNumber)")
+                    .font(.system(size: 96))
+                    .padding()
+            }
+            Stepper(value: $firstNumber, label: {
+                Text("First Number")
+            })
+            HStack{
+                Spacer()
+                Text("\(secondNumber)")
+                    .font(.system(size: 96))
+                    .padding()
+            }
+            Stepper(value: $secondNumber, label: {
+                Text("Second Number")
+            })
+        }
+    }
+}
+
+#Preview {
+    SumsView()
+}
