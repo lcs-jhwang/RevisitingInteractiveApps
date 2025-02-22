6eb5d063540ab697c6e6e40f36aa434facc7735e
Julien Hwang <jhwang@lcs.on.ca>
2025-02-21 20:07:54 -0500

    created a stepper to change the values of the base

--- RevisitingInteractiveApps/ContentView.swift
+++ RevisitingInteractiveApps/ContentView.swift
@@ -1,24 +1,46 @@
 //
 //  ContentView.swift
-//  RevisitingInteractiveApps
+//  StepperExample
 //
-//  Created by Julien Hwang on 21/2/2025.
-//
-
+ 
 import SwiftUI
-
+ 
 struct ContentView: View {
+    
+    // MARK: Stored properties
+    @State var base: Int = 1
+    
+    // MARK: Computed properties
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            
+            Spacer()
+            
+            HStack(alignment: .top) {
+ 
+                Text("\(base)")
+                    .font(.system(size: 96))
+ 
+                Text("2")
+                    .font(.system(size: 44))
+ 
+                Text("=")
+                    .font(.system(size: 96))
+ 
+                Text("25")
+                    .font(.system(size: 96))
+            }
+            
+            Stepper(value: $base, label: {Text("Base")
+            })
+                        
+            Spacer()
         }
         .padding()
     }
 }
-
+ 
 #Preview {
     ContentView()
 }
+ 
