6fd145f2c8df8f78f70fe53a29d5af988d47dbc4
Julien Hwang <jhwang@lcs.on.ca>
2025-02-21 19:58:00 -0500

    Initial Commit

---
+++ RevisitingInteractiveApps/RevisitingInteractiveAppsApp.swift
@@ -0,0 +1,17 @@
+//
+//  RevisitingInteractiveAppsApp.swift
+//  RevisitingInteractiveApps
+//
+//  Created by Julien Hwang on 21/2/2025.
+//
+
+import SwiftUI
+
+@main
+struct RevisitingInteractiveAppsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
