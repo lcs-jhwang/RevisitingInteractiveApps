8101bcb17f1a32318dce7bff534a3fa23947c370
Julien Hwang <jhwang@lcs.on.ca>
2025-02-21 20:28:15 -0500

    Made the differences View

---
+++ RevisitingInteractiveApps/Preview Content/DifferencesView.swift
@@ -0,0 +1,59 @@
+//
+//  DifferencesView.swift
+//  RevisitingInteractiveApps
+//
+//  Created by Julien Hwang on 21/2/2025.
+//
+
+import SwiftUI
+
+struct DifferencesView: View {
+    
+    //MARK: Stored properties
+    @State var firstNumber: Int = 1
+    @State var secondNumber: Int = 2
+    
+    //MARK: Computed properties
+    
+    var sum: Int{
+        return firstNumber - secondNumber
+    }
+    
+    var body: some View {
+        VStack{
+            HStack{
+                Spacer()
+                Text("\(firstNumber)")
+                    .font(.system(size: 96))
+                    .padding()
+            }
+            Stepper(value: $firstNumber, label: {
+                Text("First Number")
+            })
+            HStack{
+                Text("-")
+                    .font(.system(size: 96))
+                    .padding()
+                Spacer()
+                Text("\(secondNumber)")
+                    .font(.system(size: 96))
+                    .padding()
+            }
+            Stepper(value: $secondNumber, label: {
+                Text("Second Number")
+            })
+            
+            HStack{
+                Spacer()
+                Text("\(sum)")
+                    .font(.system(size: 96))
+                    .padding()
+            }
+            
+        }
+    }
+}
+
+#Preview {
+    DifferencesView()
+}
