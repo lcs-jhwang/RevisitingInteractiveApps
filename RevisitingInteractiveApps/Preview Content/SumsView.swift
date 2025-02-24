8b251bb7411960763a62309cdad570b1d0074053
Julien Hwang <jhwang@lcs.on.ca>
2025-02-24 14:30:46 -0500

    Made the user interface better

--- RevisitingInteractiveApps/Preview Content/SumsView.swift
+++ RevisitingInteractiveApps/Preview Content/SumsView.swift
@@ -14,7 +14,7 @@ struct SumsView: View {
     
     //MARK: Computed properties
     
-        
+    
     var body: some View {
         VStack{
             if let sum = viewModel.sum {
@@ -24,32 +24,38 @@ struct SumsView: View {
                         .font(.system(size: 96))
                         .padding()
                 }
-                Text("\(sum.result)")
-                    .font(.system(size: 96))
-                    .padding()
                 
                 HStack{
                     Spacer()
                     Text("\(sum.secondNumber)")
                         .font(.system(size: 96))
                         .padding()
+                    
                 }
                 HStack{
+                    Text("+")
+                        .font(.system(size: 96))
+                        .padding()
                     Spacer()
-                    Text(")")
+                    Text("\(sum.result)")
                         .font(.system(size: 96))
                         .padding()
                 }
-            } else {
+                
+            }
+            else {
                 // Show content unavailable view
                 ContentUnavailableView("Unable to calculate", systemImage: "sun", description: Text(viewModel.recoverySuggestion))
             }
             
-
+            
             TextField("Enter first number", text: $viewModel.providedFirstNumber)
+                .padding()
+                .font(.system(size: 40))
             
             TextField("Enter second number", text: $viewModel.providedSecondNumber)
-
+                .padding()
+                .font(.system(size: 40))
         }
     }
 }
